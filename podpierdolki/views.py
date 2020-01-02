2975c44cedc76c11b1362ea44e83ec6450369642
Przemysław Buczkowski <przemub@1mi.pl>
2020-01-03 00:49:05 +0100

    Brudy z namierzaniem (napisane kiedyś dawno)

--- podpierdolki/views.py
+++ podpierdolki/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponseNotAllowed
 from django.db.models import Q
 
+from ipware import get_client_ip
+from geoip import geolite2
+
 from .models import Brudas, Donos
 
 def main_index(request):
@@ -11,12 +14,16 @@ def main_index(request):
 def add(request):
     if request.method != "POST":
         return HttpResponseNotAllowed(request)
+	
+    ip = get_client_ip(request)
+    #print(ip, file=environ['wsgi.errors'])
+    #print("ARKA GDYNIA KURWA ŚWINIA", file=environ['wsgi.errors'])
 
     brudas = Brudas.objects.get_or_create(nazwa=request.POST['name'])[0]
     donos = Donos(obsmarowany=brudas, tytul=request.POST['tytul'], tresc=request.POST['tresc'])
     donos.save()
 
-    return render(request, 'podpierdolki/index.html', {'alert': True})
+    return render(request, 'podpierdolki/index.html', {'alert': True, 'ip': ip, 'lokacja': ip})
 
 def search(request):
     q = request.POST.get('q', '')
